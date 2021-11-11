9c81129174e47ebdafb9ac2e3d3faf8e30776764
hassanabd22 <76787879+hassanabd22@users.noreply.github.com>
2021-11-11 18:25:07 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ let store = localStorage.getItem('dark-mode');
 btn.onclick = function darkMode() {
    document.body.classList.toggle('dark-mode')
    if ( document.body.classList.contains('dark-mode')) {
-      images.src = '../images/moon-solid.svg';
+      images.src = 'images/moon-solid.svg';
    } else {
-      images.src = '../images/sun-regular.svg';
+      images.src = 'images/sun-regular.svg';
    }
 }
 
